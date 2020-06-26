a8e994e322faea706596e8215b6f726f48c7aba1
MCMicS <MCMicS@users.noreply.github.com>
2020-06-26 08:28:54 +0200

    Update src/index.ts
    Co-authored-by: Samuel Attard <samuel.r.attard@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ type ParseOptions = {
 export async function parseDocs(options: ParseOptions) {
   const packageMode = options.packageMode || 'single';
 
-  const apiDocsPath = path.resolve('./', 'docs', 'api');
+  const apiDocsPath = path.resolve(options.baseDirectory, 'docs', 'api');
   const structuresPath = path.resolve(apiDocsPath, 'structures');
 
   let structures: string[] = [];
