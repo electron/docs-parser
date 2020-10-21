fa9bfe7872d8ca394ff6e69282e2f9f52d33e2db
David Sanders <dsanders11@ucsbalum.com>
2020-10-21 12:56:31 -0700

    fix: correct GitHub repo base URL (#44)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const WEBSITE_BASE_DOCS_URL = 'https://electronjs.org';
 export const REPO_BASE_DOCS_URL = (version: string) =>
-  `https://github.com/electron/electron/blob/${version}/docs/api`;
+  `https://github.com/electron/electron/blob/${version}`;
